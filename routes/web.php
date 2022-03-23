cb9c59ceef88e1420f3f2f12a73c9c7c26c832be
ponz <ponzxp@gmail.com>
2022-03-23 21:57:17 +0900

    make_edit&delete

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/shops', [ShopController::class, 'index'])->name('shop.list');
 Route::get('/shop/new', [ShopController::class, 'create'])->name('shop.new');
 Route::post('/shop', [ShopController::class, 'store'])->name('shop.store');
+Route::get('/shop/edit/{id}', [ShopController::class, 'edit'])->name('shop.edit');
+Route::post('/shop/update/{id}', [ShopController::class, 'update'])->name('shop.update');
 
 Route::get('/shop/{id}', [ShopController::class, 'show'])->name('shop.detail');
+Route::delete('/shop/{id}', [ShopController::class, 'destroy'])->name('shop.destroy');
 
 Route::get('/', function () {
     return redirect('/shops');
