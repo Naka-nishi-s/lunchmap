d9c1c4a84453f90ddd3259f102a0096f6b293e5d
ponz <ponzxp@gmail.com>
2022-03-23 22:01:39 +0900

    make_google_map_api

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,6 +8,8 @@
   {{ $shop->address }}
 </div>
 
+<iframe id='map' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyCPDnz9KXXAz74qcpxncZiMdiFQU5jX47o&q={{ $shop->address }}' width='100%' height='320' frameborder='0'></iframe>
+
 <div>
   <a href="{{ route('shop.list') }}">一覧に戻る</a>
   | <a href="{{ route('shop.edit', ['id' => $shop->id]) }}">編集する</a>
